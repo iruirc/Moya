6e35e98f3a7feb0896947f9a74626330cac52a7c
Dmitry Nuzhin <iruirc@gmail.com>
2024-07-11 17:18:49 +0300

    Change dependency to RxSwift from 'upToNextMajor' to 'exact' version

--- Package.swift
+++ Package.swift
@@ -30,7 +30,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.0.0")),
         .package(url: "https://github.com/ReactiveCocoa/ReactiveSwift.git", .upToNextMajor(from: "6.0.0")),
-        .package(url: "https://github.com/iruirc/RxSwift.git", from: "6.7.100"),
+        .package(url: "https://github.com/iruirc/RxSwift.git", exact: "6.7.100"),
         .package(url: "https://github.com/Quick/Quick.git", .upToNextMajor(from: "4.0.0")), // dev
         .package(url: "https://github.com/Quick/Nimble.git", .upToNextMajor(from: "9.0.0")), // dev
         .package(url: "https://github.com/AliSoftware/OHHTTPStubs.git", .upToNextMajor(from: "9.0.0")) // dev
